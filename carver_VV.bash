be60d30853985feeb46a5d5e8c608597796e53e8
evanskj <evanskj@ornl.gov>
2013-08-26 03:32:54 +0000

    changes in scripts to allow code to build all 4 ways we test on hopper. Still several things to work out with LIVV on hopper since HTML permissions are different
    git-svn-id: http://oceans11.lanl.gov/svn/PISCEES/trunk@629 42a2fd4b-9c52-44de-a8a9-1772c67210cd

--- carver_VV.bash
+++ carver_VV.bash
@@ -1,29 +1,26 @@
 #!/bin/bash
 
-#KJE 1/2013 evanskj@ornl.gov
-#This is the master script to set the parameters and paths to run the LIVV kit on NERSC euclid 
+#KJE, AJB, NRM 8/2013 evanskj@ornl.gov
+#This is the master script to set the parameters and paths to run the LIVV kit on lens
 #Efforts funded by DOE BER PISCEES SciDAC project
 #Currently it is designed specifically for the GLIDE dycore of the CISM model, because it is
 #designed to read its output
 
-# current environment to be loaded on NERSC's Carver machine 1/2/2013
-#module unload netcdf/4.1.1
-#module load netcdf/4.1.3
-#module load ncar/6.0.0
-#module load nco
-#module load python
+# load these before running
+source $MODULESHOME/init/bash
+module load ncl/6.0.0
+module load nco/4.0.7
+module unload python/2.7
+module load python/2.7.3
+module load python_numpy/1.7.1
+module load python_matplotlib/1.2.1
+module load netcdf/4.1.3
 
-# and set env so that files are written with permissions to read the webpage
-umask 002 
+#define user for website
+export USERNAME=$USER
 
 #user added comment of analysis to be performed
 export COMMENT="evaluating code and test suite for CISM2.0 release"
-export USERNAME='evanskj'
-
-# flags to run the production analysis - you always run the test suite
-RUN_GIS=0 
-# ANT not activated yet, always set to 0. If 1, add -a to end of python command line at the bottom
-RUN_ANT=0 
 
 #specify location of executables 
 #/reg_test and /livv needs to be placed in the subdirectory below
@@ -35,37 +32,58 @@ export HTML_PATH="/project/projectdirs/piscees/www"
 # providing a username creates a directory by that name in the location above in which all the web files will go
 export HTML_LINK="portal.nersc.gov/project/~piscees"
 
-if [ $RUN_GIS -eq 1 ] 
-then
-#Production run and production benchmark information
-#directory of run
-  export GIS_FILEPATH="$TEST_FILEPATH/prod_test"
-#configure file
-  export GIS_CONFIG="$GIS_FILEPATH/gis_5km.config"
-#sets the filepath to the benchmark netcdf file 
-  export GIS_BENCH_NETCDF_FILEPATH="$GIS_FILEPATH/data"
-  export GIS_BENCH_NETCDF_FILENAME="gis_5km.ice2sea.init.nc"
-  export GIS_BENCH_NETCDF_FILE="$GIS_BENCH_NETCDF_FILEPATH/$GIS_BENCH_NETCDF_FILENAME"
-#production run benchmark screen output for collecting convergence information
-  export GIS_OUTPUT="$GIS_BENCH_NETCDF_FILEPATH/out.gnu"
-# right now only plots the benchmark
+# flags to select verification tests
+export RUN_DOME30_DIAGNOSTIC=1
+export RUN_DOME30_EVOLVING=0
+export RUN_CIRCULAR_SHELF=1
+export RUN_CONFINED_SHELF=1
+export RUN_ISMIP_HOM_A80=1
+export RUN_ISMIP_HOM_A20=1
+export RUN_ISMIP_HOM_C=0
+export RUN_GIS_10KM=0
 
-#sets the filepath to the production output file to be analyzed
-  export GIS_VAR_NETCDF_FILENAME="gis_5km.ice2sea.51-100.nc"
-  export GIS_VAR_NETCDF_FILE="$GIS_BENCH_NETCDF_FILEPATH/$GIS_VAR_NETCDF_FILENAME"
+# flags to run the production analysis - always run the test suite
+export RUN_GIS=0
+export RUN_ANT=0
+
+#specify location of the production GIS run if RUN_GIS is turned
+if (($RUN_GIS == 1)); then
+		#  directory of run
+		export GIS_FILEPATH="$TEST_FILEPATH/prod_test"
+		#  cofigure file
+		export GIS_CONFIG="gis_5km.config"
+		#  production run screen output for collecting convergence information
+		export GIS_OUTPUT="out.gnu"
+		#  xml file
+		export GIS_XML="trilinosOptions.xml"
+	fi
+
+if (($RUN_ANT == 1)); then
+		#  directory of run
+		export ANT_FILEPATH="$TEST_FILEPATH/ant"
+		#  cofigure file
+		export ANT_CONFIG="ant_5km.config"
+		#  production run screen output for collecting convergence information
+		export ANT_OUTPUT="out.gnu"
+	fi
 
 #TODO once list of plots created, add feature to have user pick which plots to make, default provided
-fi
 
-if [ $RUN_ANT -eq 1 ] 
-then
-#  directory of run
-  export ANT_FILEPATH="$TEST_FILEPATH/ant"
-#  cofigure file
-  export ANT_CONFIG="ant_5km.config"
-#  production run screen output for collecting convergence information
-  export ANT_OUTPUT="out.gnu"
-fi
+#From here below, the commands are set automatically and don't require changing by the user
+
+#resulting pathnames from settings given by user
+export GIS_CONFIG_FILE="$GIS_FILEPATH/$GIS_CONFIG"
+export GIS_OUTPUT_FILEPATH="$GIS_FILEPATH/data"
+export GIS_OUTPUT_FILE="$GIS_OUTPUT_FILEPATH/$GIS_OUTPUT"
+
+#sets the filepath to the benchmark netcdf file 
+export GIS_BENCH_NETCDF_FILEPATH="$GIS_FILEPATH/data"
+export GIS_BENCH_NETCDF_FILENAME="gis_5km.ice2sea.init.nc"
+export GIS_BENCH_NETCDF_FILE="$GIS_BENCH_NETCDF_FILEPATH/$GIS_BENCH_NETCDF_FILENAME"
+
+#sets the filepath to the production output file to be analyzed
+export GIS_VAR_NETCDF_FILENAME="gis_5km.ice2sea.51-100.nc"
+export GIS_VAR_NETCDF_FILE="$GIS_BENCH_NETCDF_FILEPATH/$GIS_VAR_NETCDF_FILENAME"
 
 # date stamp of LIVV run to put with comments
 NOW=$(date +"%m-%d-%Y-%r")
@@ -79,17 +97,17 @@ export NCL_PATH="$SCRIPT_PATH/plots"
 export DATA_PATH="$SCRIPT_PATH/data"
 
 #command to run python script while inputting all of the files listed above
-#NOTE: not all settings are required to run the python script
-if [ $RUN_GIS -eq 1 ] 
-then
-python $PY_PATH/VV_main.py -p "$PY_PATH" -j "$HTML_PATH" -k "$NCL_PATH" -c "$GIS_CONFIG" -o "$GIS_OUTPUT" -s "$GIS_BENCH_NETCDF_FILE" -n "$GIS_VAR_NETCDF_FILE" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -d "$DATA_PATH" -l "$HTML_LINK" -g
-
+#NOTE: not all settings are required to run the python script, type "python VV_main -h" in the command line for a full list of options
+#TODO include options if RUN_ANT is turned on, right now only have settings for GIS
+if (($RUN_GIS == 1)); then
+		python $PY_PATH/VV_main.py -d "$PY_PATH" -b "$SCRIPT_PATH" -j "$HTML_PATH" -l "$HTML_LINK" -k "$NCL_PATH" -c "$GIS_CONFIG_FILE" -o "$GIS_OUTPUT_FILE" -s "$GIS_BENCH_NETCDF_FILE" -n "$GIS_VAR_NETCDF_FILE" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -g "$GIS_FILEPATH" -D "$RUN_DOME30_DIAGNOSTIC" -E "$RUN_DOME30_EVOLVING" -I "$RUN_CIRCULAR_SHELF" -O "$RUN_CONFINED_SHELF" -A "$RUN_ISMIP_HOM_A80" -B "$RUN_ISMIP_HOM_A20" -C "$RUN_ISMIP_HOM_C" -G "$RUN_GIS_10KM"  #-a "$DATA_PATH"
 else
-python $PY_PATH/VV_main.py -p "$PY_PATH" -j "$HTML_PATH" -k "$NCL_PATH" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -d "$DATA_PATH" -l "$HTML_LINK" 
+
+		python $PY_PATH/VV_main.py -d "$PY_PATH" -b "$SCRIPT_PATH" -j "$HTML_PATH" -l "$HTML_LINK" -k "$NCL_PATH" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -D "$RUN_DOME30_DIAGNOSTIC" -E "$RUN_DOME30_EVOLVING" -I "$RUN_CIRCULAR_SHELF" -O "$RUN_CONFINED_SHELF" -A "$RUN_ISMIP_HOM_A80" -B "$RUN_ISMIP_HOM_A20" -C "$RUN_ISMIP_HOM_C" -G "$RUN_GIS_10KM"
 fi
 
 #type "python VV_main -h" in the command line for a full list of options
 
-chmod -R 2775 $HTML_PATH
-chgrp -R piscees $HTML_PATH
+chmod -R 2775 $HTML_PATH/$USER
+chgrp -R piscees $HTML_PATH/$USER
 
