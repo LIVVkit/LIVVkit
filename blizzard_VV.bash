838649da136e9ce74720f56ba78c82d9c624562d
boghozianaj <boghozianaj@ornl.gov>
2013-10-11 18:12:51 +0000

    updating bash script for blizzard
    git-svn-id: http://oceans11.lanl.gov/svn/PISCEES/trunk@736 42a2fd4b-9c52-44de-a8a9-1772c67210cd

--- blizzard_VV.bash
+++ blizzard_VV.bash
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-#KJE 1/2013 evanskj@ornl.gov
-#This is the master script to set the parameters and paths to run the LIVV kit on mac os computer
+#KJE, AJB, NRM 8/2013 evanskj@ornl.gov
+#This is the master script to set the parameters and paths to run the LIVV kit on lens
 #Efforts funded by DOE BER PISCEES SciDAC project
-#Currently it is designed specifically for the GLIDE dycore of the CISM model, because it is
-#designed to read its output
+#Currently it is designed specifically for the GLIDE dycore of the CISM model, because it is designed to read its output
+
 
 # load these before running
-#source $MODULESHOME/init/bash
+#source $MODULESHOME/init/bash #make sure to put "source $MODULESHOME/init/bash" in your .bashrc file
 #module load ncl/6.0.0
 #module load nco/4.0.7
-#module load python/2.7
+#module unload python/2.7
+#module load python/2.7.3
+#module load python_numpy/1.7.1
+#module load python_matplotlib/1.2.1
 #module load netcdf/4.1.3
+#module load netcdf4-python/1.0
 
-#set path to python version on mac
-export MY_PYTHON="python"
-
-#define user for website
+# define user for website
 USERNAME=$USER
 
-#user added comment of analysis to be performed
+# user added comment of analysis to be performed
 COMMENT="test run of code"
 
 # change to your location of livv kit
-export TEST_FILEPATH=/home/imn/PISCEES/test/piscees-trunk/tests/higher-order
+export TEST_FILEPATH="/home/$USER/piscees-trunk/tests/higher-order"
 export SCRIPT_PATH="$TEST_FILEPATH/livv"
-export DATA_DIR="data_mac"
+#data_dir changes based on what machine livv is run on (choices: titan, hopper, mac)
+export DATA_DIR="data_rhel6"
 
-#specify location where the html files will be sent so they are viewable on the web
-export HTML_PATH="/home/$USER/public_html"
+# specify location where the html files will be sent so they are viewable on the web
+# livv will create the www directory in the HTML_PATH if it does not already exist
+export HTML_PATH="/home/$USER/public_html/livv_2013_1007_16_22_27"
 mkdir -p $HTML_PATH
 export HTML_LINK="file://$HTML_PATH/livv"
 
@@ -42,22 +45,12 @@ export RUN_ISMIP_HOM_A20=1
 export RUN_ISMIP_HOM_C=0
 export RUN_GIS_10KM=0
 
-#flags to select production analysis
-export RUN_GIS=0
+# flags to select production analysis
+export GIS_LARGE_TESTS=0
+export RUN_DOME500=0
+export RUN_GIS_5KM=0
+
 export RUN_ANT=0
-
-
-#specify location of the production GIS run if RUN_GIS is turned
-if (($RUN_GIS == 1)); then
-		#  directory of run
-		export GIS_FILEPATH="$TEST_FILEPATH/gis_5km_long"
-		#  cofigure file
-		export GIS_CONFIG="gis_5km.config"
-		#  production run screen output for collecting convergence information
-		export GIS_OUTPUT="out.gnu"
-		#  xml file
-		export GIS_XML="trilinosOptions.xml"
-	fi
 
 if (($RUN_ANT == 1)); then
 		#  directory of run
@@ -68,42 +61,33 @@ if (($RUN_ANT == 1)); then
 		export ANT_OUTPUT="out.gnu"
 	fi
 
-#TODO once list of plots created, add feature to have user pick which plots to make, default provided
+# TODO once list of plots created, add feature to have user pick which plots to make, default provided
 
-#From here below, the commands are set automatically and don't require changing by the user
+# From here below, the commands are set automatically and don't require changing by the user
 
-#resulting pathnames from settings given by user
-export GIS_CONFIG_FILE="$GIS_FILEPATH/$GIS_CONFIG"
-export GIS_OUTPUT_FILEPATH="$GIS_FILEPATH/data"
-export GIS_OUTPUT_FILE="$GIS_OUTPUT_FILEPATH/$GIS_OUTPUT"
-
-#sets the filepath to the benchmark netcdf file 
-export GIS_BENCH_NETCDF_FILEPATH="$GIS_FILEPATH/data"
-export GIS_BENCH_NETCDF_FILENAME="gis_5km.ice2sea.init.nc"
-export GIS_BENCH_NETCDF_FILE="$GIS_BENCH_NETCDF_FILEPATH/$GIS_BENCH_NETCDF_FILENAME"
-
-#sets the filepath to the production output file to be analyzed
-export GIS_VAR_NETCDF_FILENAME="gis_5km.ice2sea.51-100.nc"
-export GIS_VAR_NETCDF_FILE="$GIS_BENCH_NETCDF_FILEPATH/$GIS_VAR_NETCDF_FILENAME"
+# resulting pathnames from settings given by user
+export GIS_OUTPUT_FILEPATH="$PERF_FILEPATH/data"
 
 # date stamp of LIVV run to put with comments
 NOW=$(date +"%m-%d-%Y-%r")
 echo $NOW $COMMENT
 
 # settings not generally altered, but leaving the option open for future extension
-#location where the livv code is located
+# location where the livv code is located
 export PY_PATH="$SCRIPT_PATH/bin"
-#location where the ncl directory of the ncl scripts and .nc files are located
+# location where the ncl directory of the ncl scripts and .nc files are located
 export NCL_PATH="$SCRIPT_PATH/plots"
 
-#command to run python script while inputting all of the files listed above
-#NOTE: not all settings are required to run the python script, type "python VV_main -h" in the command line for a full list of options
-#TODO include options if RUN_ANT is turned on, right now only have settings for GIS
-if (($RUN_GIS == 1)); then
-		$MY_PYTHON $PY_PATH/VV_main.py -d "$PY_PATH" -j "$HTML_PATH/" -l "$HTML_LINK" -k "$NCL_PATH" -d "$DATA_DIR" -c "$GIS_CONFIG_FILE" -o "$GIS_OUTPUT_FILE" -s "$GIS_BENCH_NETCDF_FILE" -n "$GIS_VAR_NETCDF_FILE" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -g "$GIS_FILEPATH" -D "$RUN_DOME30_DIAGNOSTIC" -E "$RUN_DOME30_EVOLVING" -I "$RUN_CIRCULAR_SHELF" -O "$RUN_CONFINED_SHELF" -A "$RUN_ISMIP_HOM_A80" -B"$RUN_ISMIP_HOM_A20" -C "$RUN_ISMIP_HOM_C" -G "$RUN_GIS_10KM"
+# command to run python script while inputting all of the files listed above
+# NOTE: not all settings are required to run the python script, type "python VV_main -h" in the command line for a full list of options
+# TODO include options if RUN_ANT is turned on, right now only have settings for GIS
+if (($GIS_LARGE_TESTS == 1)); then
+		python $PY_PATH/VV_main.py -d "$PY_PATH" -j "$HTML_PATH" -l "$HTML_LINK" -k "$NCL_PATH" -d "$DATA_DIR" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -D "$RUN_DOME30_DIAGNOSTIC" -E "$RUN_DOME30_EVOLVING" -I "$RUN_CIRCULAR_SHELF" -O "$RUN_CONFINED_SHELF" -A "$RUN_ISMIP_HOM_A80" -B "$RUN_ISMIP_HOM_A20" -C "$RUN_ISMIP_HOM_C" -G "$RUN_GIS_10KM" -F "$RUN_DOME500" -H "$RUN_GIS_5KM"
 else
 
-		$MY_PYTHON $PY_PATH/VV_main.py -d "$PY_PATH" -j "$HTML_PATH/" -l "$HTML_LINK" -k "$NCL_PATH" -d "$DATA_DIR" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -D "$RUN_DOME30_DIAGNOSTIC" -E "$RUN_DOME30_EVOLVING" -I "$RUN_CIRCULAR_SHELF" -O "$RUN_CONFINED_SHELF" -A "$RUN_ISMIP_HOM_A80" -B "$RUN_ISMIP_HOM_A20" -C "$RUN_ISMIP_HOM_C" -G "$RUN_GIS_10KM"
+		python $PY_PATH/VV_main.py -d "$PY_PATH" -j "$HTML_PATH" -l "$HTML_LINK" -k "$NCL_PATH" -d "$DATA_DIR" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -D "$RUN_DOME30_DIAGNOSTIC" -E "$RUN_DOME30_EVOLVING" -I "$RUN_CIRCULAR_SHELF" -O "$RUN_CONFINED_SHELF" -A "$RUN_ISMIP_HOM_A80" -B "$RUN_ISMIP_HOM_A20" -C "$RUN_ISMIP_HOM_C" -G "$RUN_GIS_10KM" -F "$RUN_DOME500" -H "$RUN_GIS_5KM"
 fi
 
-chmod 744 $HTML_PATH/*
+#type "python VV_main -h" in the command line for a full list of options
+
+chmod -R 775 $HTML_PATH/*
