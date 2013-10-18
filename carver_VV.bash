35205c8edf5c0f33c26bda47cb28c6b5e1ad9bee
boghozianaj <boghozianaj@ornl.gov>
2013-10-18 20:57:35 +0000

    updating changes made to carver_VV.bash file to livv can run smoothly on carver
    git-svn-id: http://oceans11.lanl.gov/svn/PISCEES/trunk@751 42a2fd4b-9c52-44de-a8a9-1772c67210cd

--- carver_VV.bash
+++ carver_VV.bash
@@ -15,7 +15,7 @@ module load python/2.7.3
 module load numpy/1.6.1
 module load matplotlib/1.1.0
 module load netcdf/4.2.0
-module load netcdf4-python/1.0.2
+module load netcdf4-python/1.0.6
 
 #define user for website
 export USERNAME=$USER
@@ -27,7 +27,7 @@ export COMMENT="evaluating code and test suite for CISM2.0 release"
 #/reg_test and /livv needs to be placed in the subdirectory below
 export TEST_FILEPATH="$GSCRATCH/higher-order"
 export SCRIPT_PATH="$TEST_FILEPATH/livv"
-export BENCH_DATA="data_hopper"
+export DATA_DIR="data_hopper"
 
 #specify location where the html files will be sent 
 export HTML_PATH="/project/projectdirs/piscees/www"
@@ -81,10 +81,10 @@ export NCL_PATH="$SCRIPT_PATH/plots"
 #NOTE: not all settings are required to run the python script, type "python VV_main -h" in the command line for a full list of options
 #TODO include options if RUN_ANT is turned on, right now only have settings for GIS
 if (($GIS_LARGE_TESTS == 1)); then
-		python $PY_PATH/VV_main.py -d "$PY_PATH" -b "$SCRIPT_PATH" -j "$HTML_PATH" -l "$HTML_LINK" -k "$NCL_PATH" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -D "$RUN_DOME30_DIAGNOSTIC" -E "$RUN_DOME30_EVOLVING" -I "$RUN_CIRCULAR_SHELF" -O "$RUN_CONFINED_SHELF" -A "$RUN_ISMIP_HOM_A80" -B "$RUN_ISMIP_HOM_A20" -C "$RUN_ISMIP_HOM_C" -G "$RUN_GIS_10KM" -F "$RUN_DOME500" -H "$RUN_GIS_5KM" #-a "$DATA_PATH"
+		python $PY_PATH/VV_main.py -b "$SCRIPT_PATH" -j "$HTML_PATH" -l "$HTML_LINK" -k "$NCL_PATH" -d "$DATA_DIR" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -D "$RUN_DOME30_DIAGNOSTIC" -E "$RUN_DOME30_EVOLVING" -I "$RUN_CIRCULAR_SHELF" -O "$RUN_CONFINED_SHELF" -A "$RUN_ISMIP_HOM_A80" -B "$RUN_ISMIP_HOM_A20" -C "$RUN_ISMIP_HOM_C" -G "$RUN_GIS_10KM" -F "$RUN_DOME500" -H "$RUN_GIS_5KM" #-a "$DATA_PATH"
 else
 
-		python $PY_PATH/VV_main.py -d "$PY_PATH" -b "$SCRIPT_PATH" -j "$HTML_PATH" -l "$HTML_LINK" -k "$NCL_PATH" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -D "$RUN_DOME30_DIAGNOSTIC" -E "$RUN_DOME30_EVOLVING" -I "$RUN_CIRCULAR_SHELF" -O "$RUN_CONFINED_SHELF" -A "$RUN_ISMIP_HOM_A80" -B "$RUN_ISMIP_HOM_A20" -C "$RUN_ISMIP_HOM_C" -G "$RUN_GIS_10KM" -F "$RUN_DOME500" -H "$RUN_GIS_5KM"
+		python $PY_PATH/VV_main.py -b "$SCRIPT_PATH" -j "$HTML_PATH" -l "$HTML_LINK" -k "$NCL_PATH" -d "$DATA_DIR" -t "$TEST_FILEPATH" -i "$NOW" -m "$COMMENT" -u "$USERNAME" -D "$RUN_DOME30_DIAGNOSTIC" -E "$RUN_DOME30_EVOLVING" -I "$RUN_CIRCULAR_SHELF" -O "$RUN_CONFINED_SHELF" -A "$RUN_ISMIP_HOM_A80" -B "$RUN_ISMIP_HOM_A20" -C "$RUN_ISMIP_HOM_C" -G "$RUN_GIS_10KM" -F "$RUN_DOME500" -H "$RUN_GIS_5KM"
 fi
 
 #type "python VV_main -h" in the command line for a full list of options
