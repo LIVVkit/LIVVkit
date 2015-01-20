f7acf9808ca46db6ee34b327b5e6cd5c7ddbaad5
arbennett <bennettar@ornl.gov>
2015-01-20 12:25:35 -0800

    Adding first draft of ismip tests.

--- livv_bin/VV_ismip.py
+++ livv_bin/VV_ismip.py
@@ -6,6 +6,12 @@ Created on Dec 8, 2014
 @author: bzq
 '''
 
+import re
+import os
+import sys
+import glob
+import subprocess
+
 import livv
 from livv import *
 import livv_bin.VV_checks as check
@@ -21,7 +27,9 @@ class Ismip(AbstractTest):
     def __init__(self):
         self.ismipTestsRun = []
         self.ismipBitForBitDetails = dict()
+        self.ismipTestFiles = []
         self.ismipTestDetails = []
+        self.ismipFileTestDetails = []
         
         self.name = "ismip"
         self.description = "Simulates steady ice flow over a surface with periodic boundary conditions"
@@ -71,6 +79,8 @@ class Ismip(AbstractTest):
         testImages.append( glob.glob(testImgDir + "/*.jpg") )
         testImages.append( glob.glob(testImgDir + "/*.svg") )
 
+        self.ismipFileTestDetails = zip(self.ismipTestFiles, self.ismipTestDetails)
+
         # Set up the template variables  
         templateVars = {"timestamp" : livv.timestamp,
                         "user" : livv.user,
@@ -80,6 +90,7 @@ class Ismip(AbstractTest):
                         "testDescription" : self.description,
                         "testsRun" : self.ismipTestsRun,
                         "bitForBitDetails" : self.ismipBitForBitDetails,
+                        "testDetails" : self.ismipFileTestDetails,
                         "imgDir" : testImgDir,
                         "testImages" : testImages}
         outputText = template.render( templateVars )
@@ -94,8 +105,27 @@ class Ismip(AbstractTest):
     #
     #    
     def runLargeA(self):
-        print("  Large ismip-hom-a test in progress....")
-        return
+        print("  Ismip-hom-A 20km test in progress....")  
+        
+        # Search for the std output files
+        files = os.listdir(livv.inputDir + livv.dataDir + '/ismip-hom-a/20km')
+        test = re.compile(".*out.*[0-9]")
+        files = filter(test.search, files)
+        
+        # Scrape the details from each of the files and store some data for later
+        for file in files:
+            self.ismipTestDetails.append(self.parse(livv.inputDir + livv.dataDir + '/ismip-hom-a/20km/' + file))
+            self.ismipTestFiles.append(file)
+        
+        # Create the plots
+        self.plot('a','20')
+
+        # Run bit for bit test
+        self.ismipBitForBitDetails = self.bit4bit('/ismip-hom-a/20km')
+        for key, value in self.ismipBitForBitDetails.iteritems():
+            print ("    {:<30} {:<10}".format(key,value))
+
+        return 0 # zero returns success
     
     #
     # Runs the evolving dome specific test case code.
@@ -103,8 +133,27 @@ class Ismip(AbstractTest):
     #
     #
     def runLargeC(self):
-        print("  Large ismip-hom-c test in progress....")
-        return
+        print("  Ismip-hom-C 20km test in progress....")  
+        
+        # Search for the std output files
+        files = os.listdir(livv.inputDir + livv.dataDir + '/ismip-hom-c/20km')
+        test = re.compile(".*out.*[0-9]")
+        files = filter(test.search, files)
+        
+        # Scrape the details from each of the files and store some data for later
+        for file in files:
+            self.ismipTestDetails.append(self.parse(livv.inputDir + livv.dataDir + '/ismip-hom-c/20km/' + file))
+            self.ismipTestFiles.append(file)
+        
+        # Create the plots
+        self.plot('c','20')
+
+        # Run bit for bit test
+        self.ismipBitForBitDetails = self.bit4bit('/ismip-hom-c/20km')
+        for key, value in self.ismipBitForBitDetails.iteritems():
+            print ("    {:<30} {:<10}".format(key,value))
+
+        return 0 # zero returns success
     
     #
     # Runs the evolving dome specific test case code.
@@ -112,8 +161,27 @@ class Ismip(AbstractTest):
     #
     #
     def runSmallA(self):
-        print("  Small ismip-hom-a test in progress....")
-        return
+        print("  Ismip-hom-A 80km test in progress....")  
+        
+        # Search for the std output files
+        files = os.listdir(livv.inputDir + livv.dataDir + '/ismip-hom-a/80km')
+        test = re.compile(".*out.*[0-9]")
+        files = filter(test.search, files)
+        
+        # Scrape the details from each of the files and store some data for later
+        for file in files:
+            self.ismipTestDetails.append(self.parse(livv.inputDir + livv.dataDir + '/ismip-hom-a/80km/' + file))
+            self.ismipTestFiles.append(file)
+        
+        # Create the plots
+        self.plot('a','80')
+
+        # Run bit for bit test
+        self.ismipBitForBitDetails = self.bit4bit('/ismip-hom-a/80km')
+        for key, value in self.ismipBitForBitDetails.iteritems():
+            print ("    {:<30} {:<10}".format(key,value))
+
+        return 0 # zero returns success
     
     #
     # Runs the evolving dome specific test case code.
@@ -121,5 +189,32 @@ class Ismip(AbstractTest):
     #
     #
     def runSmallC(self):
-        print("  Small ismip-hom-c test in progress....")
-        return
+        print("  Ismip-hom-C 80km test in progress....")  
+        
+        # Search for the std output files
+        files = os.listdir(livv.inputDir + livv.dataDir + '/ismip-hom-c/80km')
+        test = re.compile(".*out.*[0-9]")
+        files = filter(test.search, files)
+        
+        # Scrape the details from each of the files and store some data for later
+        for file in files:
+            self.ismipTestDetails.append(self.parse(livv.inputDir + livv.dataDir + '/ismip-hom-c/80km/' + file))
+            self.ismipTestFiles.append(file)
+        
+        # Create the plots
+        self.plot('c','80')
+
+        # Run bit for bit test
+        self.ismipBitForBitDetails = self.bit4bit('/ismip-hom-c/80km')
+        for key, value in self.ismipBitForBitDetails.iteritems():
+            print ("    {:<30} {:<10}".format(key,value))
+
+        return 0 # zero returns success
+
+    #
+    # Create the plots
+    #
+    #
+    def plot(self, aOrC, size):
+        print "    Generating ismip-hom-" + aOrC + "-" + size + "km plot...." 
+        print "    This is just a dummy method.  Updates coming soon!"
