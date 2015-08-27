96c9cb429d9d9b888568caa99b59ad7c44437bb0
Joseph H Kennedy <kennedyjh@ornl.gov>
2015-08-27 12:44:02 -0400

    Update lvargo13 validation test to plot SMB
    This is a working example of running (part) of @lvargo13 NCL validation
    script. Right now, it only makes the SMB plot.
    
    Also, minor whitespace adjustment in
    `web/templates/compontents/plots_section.html`

--- validation/lvargo13/lvargo13.py
+++ validation/lvargo13/lvargo13.py
@@ -51,35 +51,41 @@ class Test(AbstractTest):
         Produces both an overall coverage percentage metric and a coverage plot.
     
         Required args:
-            plot_file:  full path to the ncl script used to plot the data
+            plot_script:  full path to the ncl script used to plot the data
             model_data: full path to the output from the model data
             bench_data: full path to the output from the benchmark data
         """
         self.description = kwargs.get('description')
-        plot_file = kwargs.get('plot_file')
-        bench_data = kwargs.get('bench_data')
-        model_data = kwargs.get('model_data')
-    
-        if not (os.path.exists(model_data) and os.path.exists(bench_data)):
+        plot_script = kwargs.get('plot_script')
+        
+        gl_data = kwargs.get('gl_data')
+        model_dir = kwargs.get('model_dir')
+        model_prefix = kwargs.get('model_prefix')
+        model_suffix = kwargs.get('model_suffix')
+        model_start = kwargs.get('model_start')
+        model_end = kwargs.get('model_end')
+        
+
+        if not (os.path.exists(gl_data) and os.path.exists(model_dir)):
             # Add more handling here -- what do we want to return for failed tests
-            print("ERROR: Could not find necessary data to run the coverage validation!")
-            print(model_data)
-            print(bench_data)
+            print("ERROR: Could not find necessary data to run the lvargo13 validation!")
+            print(gl_data)
+            print(model_dir)
             print("")
             return
     
         # Generate the script
-        output_path = util.variables.index_dir + os.sep + 'validation' + os.sep + self.name + os.sep + 'imgs' + os.sep + 'lvargo13.png'
-        self.plot_lvargo13(plot_file, model_data, bench_data, output_path)
+        output_file = util.variables.index_dir + os.sep + 'validation' + os.sep + self.name + os.sep + 'imgs' + os.sep + 'lvargo13.png'
+        self.plot_lvargo13(plot_script, gl_data, model_dir, model_prefix, model_suffix, model_start, model_end, output_file)
     
     
-    def plot_lvargo13(self, plot_file, model_data, bench_data, output_file):
+    def plot_lvargo13(self, plot_script, gl_data, model_dir, model_prefix, model_suffix, model_start, model_end, output_file):
         """ 
         Calls the ncl script to generate the plots for percent ice sheet 
         coverage.
     
         Args:
-            plot_file: Location of the ncl script to generate the coverage plot
+            plot_script: Location of the ncl script to generate the coverage plot
             model_data: The dataset with the model output
             bench_data: The dataset with the benchmark output
             output_file: The full path of where to write the plot to
@@ -87,7 +93,13 @@ class Test(AbstractTest):
         Returns:
             TBD
         """
-        ncl_command = 'ncl \'bench = addfile("'+ bench_data +'", "r")\' \'model = addfile("'+ model_data +'", "r")\' \'plotFile = "'+ output_file +'"\' ' + plot_file
+        ncl_command = 'ncl \'gl_data = addfile("'+ gl_data +'", "r")\' '  \
+                           + '\'model_prefix = "'+ os.path.join(model_dir, model_prefix) +'"\' '     \
+                           + '\'model_suffix = "'+ model_suffix +'"\' '     \
+                           + '\'model_start = '+ model_start +'\' '       \
+                           + '\'model_end = '+ model_end +'\' '           \
+                           + '\'plot_file = "'+ output_file +'"\' '       \
+                           + plot_script
     
         # Be cautious about running subprocesses
         call = subprocess.Popen(ncl_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
