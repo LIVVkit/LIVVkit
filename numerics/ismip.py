e2fa6b922ac8fa4bdf10c7412dfa00e8d3c0dea4
arbennett <bennett.andr@gmail.com>
2015-11-01 21:00:21 -0800

    Adding code for ISMIP numerical verification.  Still some issues with the data layout that need to be resolved.

--- numerics/ismip.py
+++ numerics/ismip.py
@@ -33,6 +33,7 @@ Master module for ISMIP numerics.
 import os
 import fnmatch
 import numpy as np
+from netCDF4 import Dataset
 
 from numerics.base import AbstractTest
 import util.variables
@@ -73,23 +74,67 @@ class Test(AbstractTest):
         """
         Details here
         """
-        fpath = ""
-        data_files = sorted(set(fn for fn in fnmatch.filter(os.listdir(self.data_dir), 'ismip-hom-a.*.txt')))
-        #x,y,vx_mean,vx_stdev,vy_mean,vy_stdev = np.loadtxt(fpath, unpack=True, delimiter=',', skiprows=1, usecols=(0,1,2,3,6,7)) 
+        # Grab the ISMIP benchmark data & calculate velocity norm mean and standard deviation
+        # TODO: There is a rather large issue with the way that the assimilated data is laid out
+        #       This data comes in (x,y,var1,var2,...,varn) form, meaning when we get the data in
+        #       from the file we end up with a 1xN (or Nx1, whichever) for each column.  The CISM
+        #       output data is in grid format, so each variable has an NxM array.
+        #
+        #       It will probably be easier to remap the assimilated data - this could be done one
+        #       of two ways.  The first way would involve more work on the assimilation scripts.
+        #       Instead of outputting plain text, it would be nice if the data were output in 
+        #       netcdf files that have the same structure as the CISM output.  This would make,
+        #       it easier to compare as well as well as making the files more easily human 
+        #       readable.  it would also reduce the amount of processing that we would need to 
+        #       do inside of LIVV.  Okay, this will be a WIP.
+        fpath = os.path.join(util.variables.cwd,"numerics","data","ismip-hom-a."+resolution+".lmla.txt")
+        x,y,vx_mean,vx_stdev,vy_mean,vy_stdev = np.loadtxt(fpath, unpack=True, delimiter=',', skiprows=1, usecols=(0,1,2,3,6,7)) 
+        vnorm_mean = np.sqrt(np.add(np.power(vx_mean,2), np.power(vy_mean,2)))
+        vnorm_stdev = np.sqrt(np.add(np.power(vx_stdev,2), np.power(vy_stdev,2)))
+        vnorm_plus = np.add(vnorm_mean, vnorm_stdev) 
+        vnorm_minus = np.subtract(vnorm_mean, vnorm_stdev)
 
+        # Grab the model data
+        data_files = sorted(set(fn for fn in fnmatch.filter(os.listdir(self.model_dir), 'ismip-hom-a.'+resolution+'.????.out.nc')))
+        for fname in data_files:
+            print os.path.join(self.model_dir,fname)
+            dataset = Dataset(os.path.join(self.model_dir,fname),'r')
+            uvel = dataset.variables['uvel']
+            vvel = dataset.variables['vvel']
+            vnorm = np.sqrt(np.add(np.power(uvel,2), np.power(vvel,2)))
+            under = np.subtract(vnorm,vnorm_minus)
+            over = np.subtract(vnorm_plus,vnorm)
+            bad_data = np.add(over<0, under<0)
+            mean_diff = np.subtract(velnorm, vnorm_mean)
 
     def run_experiment_c(self, resolution):
         """
         Details here
         """
-        pass
-        data_files = sorted(set(fn for fn in fnmatch.filter(os.listdir(self.data_dir), 'ismip-hom-c.*.txt')))
+        # Grab the ISMIP benchmark data & calculate velocity norm mean and standard deviation
+        fpath = os.path.join(util.variables.cwd,"numerics","data","ismip-hom-c."+resolution+".lmla.txt")
+        x,y,vx_mean,vx_stdev,vy_mean,vy_stdev = np.loadtxt(fpath, unpack=True, delimiter=',', skiprows=1, usecols=(0,1,2,3,6,7)) 
+        vnorm_mean = np.sqrt(vx_mean + vy_mean)
+        vnorm_stdev = np.sqrt(vx_stdev + vy_stdev)
+        
+        # Grab the model data
+        data_files = sorted(set(fn for fn in fnmatch.filter(os.listdir(self.model_dir), 'ismip-hom-c.'+resolution+'.????.out.nc')))
+        print data_files
+
 
 
     def run_experiment_f(self, resolution):
         """
         Details here
         """
-        pass
-        data_files = sorted(set(fn for fn in fnmatch.filter(os.listdir(self.data_dir), 'ismip-hom-f.*.txt')))
+        # Grab the ISMIP benchmark data & calculate velocity norm mean and standard deviation
+        fpath = os.path.join(util.variables.cwd,"numerics","data","ismip-hom-f."+resolution+".lmla.txt")
+        x,y,vx_mean,vx_stdev,vy_mean,vy_stdev = np.loadtxt(fpath, unpack=True, delimiter=',', skiprows=1, usecols=(0,1,2,3,6,7)) 
+        vnorm_mean = np.sqrt(vx_mean + vy_mean)
+        vnorm_stdev = np.sqrt(vx_stdev + vy_stdev)
+        
+        # Grab the model data
+        data_files = sorted(set(fn for fn in fnmatch.filter(os.listdir(self.model_dir), 'ismip-hom-f.'+resolution+'.????.out.nc')))
+        print data_files
+
 
