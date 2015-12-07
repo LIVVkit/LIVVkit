d8557861f31c50c108639a84d624c91c8fc59496
Joseph H Kennedy <kennedyjh@ornl.gov>
2015-12-07 16:43:48 -0700

    Add a yellowstone module setup file.

---
+++ setup_yellowstone.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This combination of modules worked as of:
+#     December 7 2015
+# You may need to adjust these as systems change.
+
+module load python
+module load numpy
+module load matplotlib
+module load netcdf
+module load hdf5
+module load netcdf4python
+module load nco
+module load ncl
+
+module list
