1cb2cf54da739b84299816bed4346b4848b7bb63
arbennett <bennett.andr@gmail.com>
2016-05-06 11:27:16 -0700

    Adding setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='livvkit',
+      version='1.0',
+      description='V&V library and toolkit for ice sheet models.',
+      url='http://github.com/LIVVkit/LIVVkit',
+      author='Oak Ridge National Laboratory',
+      author_email='evanskj@ornl.gov',
+      license='BSD',
+      packages=['livvkit'],
+      zip_save=False)
+
